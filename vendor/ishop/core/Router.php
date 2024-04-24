47a46108b30f957c0a82d732c96f60c9a8a51042
MantasLit <gelezin21@gmail.com>
2024-04-24 20:44:12 +0300

    Added: Router functional / Fixed: Some issues

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -7,4 +7,37 @@ namespace ishop;
 class Router
 {
 
+    protected static $routes = [];
+    protected static $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+
+    public static function getRoute()
+    {
+        return self::$routes;
+    }
+
+    public static function dispatch($url)
+    {
+        if (self::matchRoute($url)) {
+            echo "ok";
+        } else {
+            echo "no";
+        }
+    }
+
+    public static function matchRoute($url)
+    {
+        return true;
+    }
+
 }
