41b8f2ccc0bd1ef87bf2ba01db93599ff0e5a921
MantasLit <gelezin21@gmail.com>
2024-04-30 21:37:06 +0300

    Updated: View logic

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,19 +9,7 @@ class MainController extends AppController
 {
     public function indexAction()
     {
-        $posts = \R::findAll('test');
         $this->setMeta('Main page', 'Main site desc', 'website,shop,www ');
-
-        $name = 'Jhon';
-        $age = 333;
-        $names = ['Andrey', 'Jane','Mike'];
-        $cache = Cache::instance();
-        $data = $cache->get('test');
-        if(!$data){
-           $data = $cache->set('test',$names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 
 
--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,8 +1,1 @@
-<h1>Hello world!</h1>
 
-<p><?= $name ?></p>
-<p><?= $age ?></p>
-<?= debug($names); ?>
-<?php foreach ($posts as $post) : ?>
-    <h1><?= $post->title; ?></h1>
-<?php endforeach; ?>
